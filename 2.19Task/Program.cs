2bb95f25fb0b322a0a1674abd9e6cb6719605bdb
talistalis <tsarst@gmail.com>
2022-09-01 22:11:41 +0300

    add

--- 2.19Task/Program.cs
+++ 2.19Task/Program.cs
@@ -10,7 +10,31 @@ namespace _2._19Task
     {
         static void Main(string[] args)
         {
+            // Составить программу расчёта значения функций при любых значениях a и b
 
+            Console.Write("Введите значение a = ");
+            
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение b = ");
+
+            double b = double.Parse(Console.ReadLine());
+            
+            double firstSummandDivisible = 2 / (Math.Pow(a, 2) + 25);
+
+            double divisible = firstSummandDivisible + b;
+
+            double secondSummandDivider = (a + b) / 2;
+
+            double divider = Math.Sqrt(b) + secondSummandDivider;
+
+            double x = divisible / divider;
+
+            Console.WriteLine("Значение функции x = " + x);
+
+            double y = (Math.Abs(a) + 2 * Math.Sin(b)) / (5.5 * a);
+
+            Console.WriteLine("Значение функции y = " + y);
         }
     }
 }
